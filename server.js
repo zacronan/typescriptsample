5ddb9c9061c4d5b5c613ca9418ee47badb43ace5
Noh yeon jin <yjnoh@adtkorea.com>
2017-08-09 18:36:25 +0900

    init

---
+++ server.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+"use strict";
+
+var express = require('express');
+var path = require('path');
+var open = require('open');
+
+const port = 3000;
+const app = express();
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(port, function(err) {
+    if(err) {
+        console.log(err);
+    } else {
+        open('http://localhost:' + port);
+    }
+});
